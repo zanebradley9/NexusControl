aac0365a765fc0c7877ae9ecaf9312b58cd9c795
zanebradley9 <zanibradley97@gmail.com>
2026-07-01 10:27:13 -0400

    update

--- backend/config/env.js
+++ backend/config/env.js
@@ -6,4 +6,6 @@ export const ENV = {
     PORT: process.env.PORT || 5000,
     CLIENT_URL: process.env.CLIENT_URL,
     JWT_SECRET: process.env.JWT_SECRET,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
 };
